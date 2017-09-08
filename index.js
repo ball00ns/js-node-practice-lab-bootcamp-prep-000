17e0f946c4527dfe27308328a7c4b35ac67de239
Jyn Ho <jynnn@live.com>
2017-09-08 14:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Yourself"
+var height = "12093"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+console.log(parseInt(height)+1)
